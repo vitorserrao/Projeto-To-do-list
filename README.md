ce25e222a4734ef1f729307c7a13fd84a9e0b3d9
Vitor Serrão <vitorwss@gmail.com>
2022-04-17 19:34:11 -0300

    todas as funcionalidades

---
+++ README.md
@@ -0,0 +1,21 @@
+# CompraFácil
+
+Compre fácil é uma ferramenta que facilita e organiza as compras do cliente de uma maneira rápida e simples.
+
+## 📋 Pré-requisitos
+
+Um navegador atualizado.
+
+### 🛠️ Construído com
+
+- [HTML]- Criação dos elementos visuais.
+- [CSS] - Usado para estilizar a aplicação.
+- [JAVASCRIPT] - Usado para adicionar inteligência na aplicação.
+
+#### 📌 Versão
+
+1.0.
+
+## ✒️ Autor
+
+- **Vitor Serrão** - (https://github.com/vitorwss)
--- js/main.js
+++ js/main.js
@@ -1,3 +1,103 @@
-import { addItem } from './add_item.js';
-import { poup } from './poup.js';
-addItem();
+import { criaId } from './cria_id.js';
+
+let btInserirItem = document.querySelector('#btInserirItem');
+let itemDigitado = document.querySelector('#btCampoItem');
+let listaComItem = document.querySelector('#listaComItem');
+let popup = document.querySelector('#popup');
+let valorDigitado = document.querySelector('.valorDigitado'); //preço do produto
+
+const localLista = JSON.parse(localStorage.getItem('listaItem'));
+let listaItem = localStorage.getItem('listaItem') !== null ? localLista : [];
+
+const setLocalList = () => {
+  localStorage.setItem('listaItem', JSON.stringify(listaItem));
+};
+
+// cria os elementos e add na tela
+const criaElemento = (produto, id, estado) => {
+  const li = document.createElement('li');
+  let btEx = document.createElement('button');
+  let check = `<input type="checkbox" data-id=${id} ${estado}>`;
+  btEx.setAttribute('class', 'btExcluir');
+  btEx.setAttribute('id', `${id}`);
+  btEx.innerHTML = 'X';
+  li.innerHTML = `${check} <span id="item">${produto}</span>`;
+  li.appendChild(btEx);
+  listaComItem.appendChild(li);
+};
+
+let excluiItem = () => {
+  listaComItem.addEventListener('click', (evento) => {
+    //buscaIndice prcura na listaItem qual indice tem o id que foi clicado
+    const alvoClick = evento.target;
+    const buscaIndice = listaItem.forEach((obj, i) => {
+      if (alvoClick.type === 'submit' && obj.id == alvoClick.id) {
+        listaComItem.innerHTML = '';
+        listaItem.splice(i, 1);
+        setLocalList();
+        lerlistaItem();
+      }
+    });
+  });
+};
+// ler array com item a serem criados
+const lerlistaItem = () => {
+  listaItem.forEach((obj) => {
+    return criaElemento(obj.produto, obj.id, obj.estado);
+  });
+};
+
+// checkbox verifica o estado do item e modifica no array
+// pois isso ele modifica o estilo da div aparecendo/desaparecendo o popup
+//
+const checkbox = () => {
+  listaComItem.addEventListener('click', (evento) => {
+    const click = evento.target;
+    const procuraIndice = listaItem.forEach((obj, i) => {
+      if (click.type === 'checkbox' && obj.id == click.dataset.id) {
+        (listaItem[i].estado = listaItem[i].estado === '' ? 'checked' : ''), (popup.style.display = 'block');
+        setLocalList();
+        popup.onclick = () => {
+          if (valorDigitado.value) {
+            listaItem[i].valor = valorDigitado.value;
+            setLocalList();
+            valorDigitado.value = '';
+            popup.style.display = 'none';
+            console.log(listaItem[i].valor.type);
+          }
+        };
+      }
+    });
+  });
+};
+
+const calcValor = () => {
+  let calc = listaItem.reduce((total, obj) => {
+    return total + Number(obj.valor);
+  }, 0);
+};
+checkbox();
+const clickBtInserir = (evento) => {};
+btInserirItem.onclick = () => {
+  if (itemDigitado.value) {
+    listaComItem.innerHTML = '';
+    let item = {
+      id: criaId(),
+      produto: itemDigitado.value,
+      estado: '',
+      valor: '',
+    };
+    listaItem.push(item);
+    setLocalList();
+    lerlistaItem();
+    console.log(listaItem);
+    itemDigitado.value = '';
+
+    //poup();
+    // btEx.onclick = () => {
+    // listaComItem.removeChild(li);
+    //};
+  }
+};
+excluiItem();
+lerlistaItem();
